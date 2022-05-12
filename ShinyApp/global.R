6f7b9d555529fdb7d0e588cf4e970e8b7d977c71
Aditi Reddy <aditimayareddy@gmail.com>
2022-05-12 10:46:32 -0400

    add leaflet interactivity

--- ShinyApp/global.R
+++ ShinyApp/global.R
@@ -9,11 +9,26 @@ library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(maps)
+library(DT)
+library(leaflet)
+theme_set(theme_minimal())
+library(vroom) #fast data reading/importing
+library(sf) #spatial data
+library(tigris) #geojoin
+library(htmlwidgets) #interactive map labels
 
 #loading cleaned data sets
 crash <- read.csv("./data/crashes_clean.csv", stringsAsFactors = FALSE) #cleaned crash data involving at least one bicycle between 2012-2022
 reqs <- read.csv("./data/bike_reqs_clean.csv", stringsAsFactors = FALSE) #Safety requests shared by bikers
-#w <- read.csv("./data/Wards_from_2022.csv", stringsAsFactors = TRUE)
+
+#loading shapefiles for wards
+wards <- st_read("./data/wards/Wards_from_2022.shp")
+
+
+##Modifying Raw Data
+wards <- wards %>% select(ward=WARD,
+                          name=NAME
+)
 
 #getting base map
 dcmap <- get_stamenmap(
--- ShinyApp/preProc.R
+++ ShinyApp/preProc.R
@@ -62,10 +62,10 @@ crash <- crash %>% select(lat=LATITUDE,
                             lon=LONGITUDE,
                             ward=WARD,
                             rdate=REPORTDATE,
-                            major_inj=MAJORINJURIES_BICYCLIST,
-                            minor_inj=MINORINJURIES_BICYCLIST,
-                            unknown_inj=UNKNOWNINJURIES_BICYCLIST,
-                            fatal=FATAL_BICYCLIST,
+                            major_injury=MAJORINJURIES_BICYCLIST,
+                            minor_injury=MINORINJURIES_BICYCLIST,
+                            unknown_injury=UNKNOWNINJURIES_BICYCLIST,
+                            fatality=FATAL_BICYCLIST,
                             total_b=TOTAL_BICYCLES,
                             total_v=TOTAL_VEHICLES,
                             imp_b=BICYCLISTSIMPAIRED,
@@ -82,15 +82,77 @@ crash$day <-day(crash$rdate)
 crash <- crash %>% filter(year %in% 2012:2022 & total_b > 0)
 
 #adding categorical variable for injury/fatality
-crash$noinj <- 0.5
+crash$no_injury <- 0.5
 col_vec <- c(5,6,7,8,18)
 
 crash$injtype <- names(crash[col_vec])[max.col(crash[col_vec])]
 
+crash.injtype <- ordered(crash, levels = c("fatality", "major_injury", "minor_injury", "unknown_injury", "no_injury"))
+is.factor(crash.injtype)
+
 #removing dummy variable columns
-crash <- select(crash, -rdate, -minor_inj, -major_inj, -unknown_inj, -noinj)
+#crash <- select(crash, -rdate, -minor_injury, -major_injury, -unknown_injury, -no_injury, -fatality)
 
 #exporting clean crash data to csv
 write.csv(crash,"./data/crashes_clean.csv", row.names = FALSE)
 
 
+m <- leaflet(data = reqs) %>% setView(lng = -77.0369, lat = 38.9072, zoom = 12)
+m %>% addTiles() %>%
+  addMarkers(~lon, ~lat, popup = ~as.character(comments), label = ~as.character(reqtype),
+             clusterOptions = markerClusterOptions(freezeAtZoom = 12))
+
+# Show first 20 rows from the `reqs
+leaflet(data = reqs[1:20,]) %>% addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(comments), label = ~as.character(reqtype))
+
+getColor <- function(crash) {
+  sapply(crash$injtype, function(injtype) {
+    if(injtype == "no_injury") {
+      "green"
+    } else if(injtype == "minor_injury"){
+      "yellow"
+    } else if(injtype = "major_injury"){
+      "orange"
+    } else if(injtype = "major_injury"){
+      "red"
+    } else if(injtype = "unknown_injury"){
+      "tan"
+    }
+    })
+}
+
+getColor <- function(crash) {
+  sapply(crash$injtype, function(injtype) {
+    if(injtype == no_injury) {
+      "green"
+    } else if(injtype == minor_injury | injtype == major_injury | injtype == unknown_injury) {
+      "orange"
+    } else if(injtype == fatality){
+      "red"
+    } })
+}
+
+icons <- awesomeIcons(
+  icon = 'bicycle_outline',
+  iconColor = 'black',
+  library = 'ion',
+  #markerColor = getColor(crash)
+)
+
+r <- leaflet(data = crash) %>% setView(lng = -77.0369, lat = 38.9072, zoom = 12)
+r %>% addTiles() %>%
+  addAwesomeMarkers(~lon, ~lat, icon = icons, popup = ~as.character(rdate), label = ~as.character(injtype),
+             clusterOptions = markerClusterOptions())
+
+
+getColor <- function(crash) {
+  sapply(crash$injtype, function(injtype) {
+    if(injtype == no_injury) {
+      "green"
+    } else if(injtype == minor_injury | injtype == major_injury | injtype == unknown_injury) {
+      "orange"
+    } else if(injtype == fatality){
+      "red"
+    } })
+}
